cae019d5c6d98aabe8a844a3a07e6bac64bc09e8
Kyle <kyle201817147@gmail.com>
2019-03-11 18:59:43 +0800

    add the top TextView

--- app/src/main/java/com/kyle/heathcare/MainActivity.java
+++ app/src/main/java/com/kyle/heathcare/MainActivity.java
@@ -1,7 +1,6 @@
 package com.kyle.heathcare;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,33 +16,42 @@ import com.kyle.heathcare.fragment_package.HomepageFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private Fragment fragment;
+    // TODO: 2019/3/11 1)四个Fragment
+    // TODO: 2019/3/11 2)异常判断及异常Fragment（响铃、倒计时、电话权限）
+    // TODO: 2019/3/11 3)注册
+    // TODO: 2019/3/11 4)多账户数据库
+    // TODO: 2019/3/11 5)健康数据数据库
+    // TODO: 2019/3/11 6)本地偏好设置
+
+    private HomepageFragment homepageFragment;
+    private HealthFragment healthFragment;
+    private DrivingFragment drivingFragment;
+    private CenterFragment centerFragment;
+    private TextView toolbarTitle;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        public boolean onNavigationItemSelected(MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_homepage:
-                    mTextMessage.setText(R.string.title_homepage);
-                    replaceFragment(new HomepageFragment());
+                    toolbarTitle.setText(R.string.title_homepage);
+                    replaceFragment(homepageFragment);
                     return true;
 
                 case R.id.navigation_health:
-                    mTextMessage.setText(R.string.title_health);
-                    replaceFragment(new HealthFragment());
+                    toolbarTitle.setText(R.string.title_health);
+                    replaceFragment(healthFragment);
                     return true;
 
                 case R.id.navigation_driving:
-                    mTextMessage.setText(R.string.title_driving);
-                    replaceFragment(new DrivingFragment());
+                    toolbarTitle.setText("行驶记录");
+                    replaceFragment(drivingFragment);
                     return true;
 
                 case R.id.navigation_center:
-                    mTextMessage.setText(R.string.title_center);
-                    replaceFragment(new CenterFragment());
+                    toolbarTitle.setText(R.string.title_center);
+                    replaceFragment(centerFragment);
                     return true;
             }
             return false;
@@ -53,18 +61,29 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.main_frag, fragment);
+        transaction.replace(R.id.frame_frag, fragment);
         transaction.commit();
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homepageFragment = new HomepageFragment();
+        healthFragment = new HealthFragment();
+        drivingFragment = new DrivingFragment();
+        centerFragment = new CenterFragment();
+        toolbarTitle = findViewById(R.id.toolbar_title);
 
-        mTextMessage = findViewById(R.id.message);
+        // TODO: 2019/3/11 间距不当
+        toolbarTitle.setText(R.string.title_homepage);
+
+        replaceFragment(homepageFragment);
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
