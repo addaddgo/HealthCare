f603dc094696166ecc2449f22ee5ccb3f3fefd19
Kyle <kyle201817147@gmail.com>
2019-03-11 16:28:37 +0800

    init

---
+++ app/src/main/java/com/kyle/heathcare/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.kyle.heathcare;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends BaseActivity {
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        accountEdit = findViewById(R.id.account);
+        passwordEdit = findViewById(R.id.password);
+        checkBox = findViewById(R.id.remember_password);
+        Boolean isRemember = pref.getBoolean("remember_password", false);
+        login = findViewById(R.id.login);
+        if (isRemember) {
+            accountEdit.setText(pref.getString("account", ""));
+            passwordEdit.setText(pref.getString("password", ""));
+            checkBox.setChecked(true);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if (account.equals("Kyle") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if(checkBox.isChecked()){
+                        editor.putString("account",account );
+                        editor.putString("password", password);
+                        editor.putBoolean("remember_password",true);
+                    }else{
+                        editor.clear();
+                    }
+                    editor.apply();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "invalid account or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
