0c32a8d7a59457d3ea9a037be736e9c59df2ea81
Kyle <kyle201817147@gmail.com>
2019-03-14 19:22:36 +0800

    优化注册流程

--- app/src/main/java/com/kyle/healthcare/MainActivity.java
+++ app/src/main/java/com/kyle/healthcare/MainActivity.java
@@ -105,6 +105,7 @@ public class MainActivity extends BaseActivity {
         } else if (!mBluetoothAdapter.isEnabled()) {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }else{
             mChatService = new BluetoothChatService(this, mHandler);
         }
     }
