b1ef45f8fbc41cb9c2e56e2932994eae6d6c1da2
Kyle <kyle201817147@gmail.com>
2019-03-14 18:31:03 +0800

    修复了上个版本vsg文件夹存在drawable导致编译失败的问题 优化注册流程

--- app/src/main/java/com/kyle/healthcare/base_package/SignUpActivity.java
+++ app/src/main/java/com/kyle/healthcare/base_package/SignUpActivity.java
@@ -1,8 +1,8 @@
 package com.kyle.healthcare.base_package;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,13 +15,25 @@ import com.kyle.healthcare.database.user_info.User;
 
 import org.litepal.LitePal;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView man_ic,woman_ic;
-    private TextView man_tv,woman_tv;
-    private Button sign_up;
+    private int viewIndex = 1;
 
+    private ImageView man_ic, woman_ic;
+    private TextView man_tv, woman_tv;
     boolean gender;
+    private EditText et_name;
+
+    private EditText et_phone;
+    private EditText et_pass;
+
+    private EditText et_car;
+    private EditText et_id;
+
+    private Button sign_up;
+    private Button forward;
+    private Button backward;
+
     boolean isChecked;
 
     private String name;
@@ -30,56 +42,42 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private String carNumber;
     private String idNumber;
 
-    private EditText et_name;
-    private EditText et_phone;
-    private EditText et_pass;
-    private EditText et_car;
-    private EditText et_id;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-
-        man_ic = findViewById(R.id.man_ic);
-        man_tv = findViewById(R.id.man_tv);
-        woman_ic = findViewById(R.id.woman_ic);
-        woman_tv = findViewById(R.id.woman_tv);
-        sign_up = findViewById(R.id.bt_sign_up);
-
-        et_name = findViewById(R.id.user_name);
-        et_phone = findViewById(R.id.user_phone_number);
-        et_pass = findViewById(R.id.user_password);
-        et_car = findViewById(R.id.user_car_number);
-        et_id = findViewById(R.id.user_id_number);
-
-        man_ic.setOnClickListener(this);
-        woman_ic.setOnClickListener(this);
-        sign_up.setOnClickListener(this);
+        changeView(0);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.sign_up_forward_1:
+                if (checkValid()) {
+                    changeView(1);
+                }
+                break;
+            case R.id.sign_up_backward:
+                changeView(-1);
+                break;
             case R.id.man_ic:
                 isChecked = true;
                 gender = true;
                 man_ic.setImageResource(R.drawable.ic_man_c);
-                man_tv.setTextColor(Color.rgb(68,68,68));
+                man_tv.setTextColor(Color.rgb(68, 68, 68));
                 woman_ic.setImageResource(R.drawable.ic_woman);
-                woman_tv.setTextColor(Color.rgb(195,195,195));
+                woman_tv.setTextColor(Color.rgb(195, 195, 195));
                 break;
             case R.id.woman_ic:
                 isChecked = true;
                 gender = false;
                 man_ic.setImageResource(R.drawable.ic_man);
-                man_tv.setTextColor(Color.rgb(195,195,195));
+                man_tv.setTextColor(Color.rgb(195, 195, 195));
                 woman_ic.setImageResource(R.drawable.ic_woman_c);
-                woman_tv.setTextColor(Color.rgb(68,68,68));
+                woman_tv.setTextColor(Color.rgb(68, 68, 68));
                 break;
             case R.id.bt_sign_up:
-                if(checkValid()){
+                if (checkValid()) {
                     addUser();
                     finish();
                 }
@@ -89,37 +87,94 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    private void changeView(int offset) {
+        viewIndex += offset;
+        if (viewIndex <= 0) {
+            viewIndex = 1;
+        }
+        if (viewIndex >= 4) {
+            viewIndex = 3;
+        }
+        switch (viewIndex) {
+            case 1:
+                setContentView(R.layout.activity_sign_up_1);
+                man_ic = findViewById(R.id.man_ic);
+                man_tv = findViewById(R.id.man_tv);
+                woman_ic = findViewById(R.id.woman_ic);
+                woman_tv = findViewById(R.id.woman_tv);
+                man_ic.setOnClickListener(this);
+                woman_ic.setOnClickListener(this);
+                et_name = findViewById(R.id.user_name);
+                forward = findViewById(R.id.sign_up_forward_1);
+                forward.setOnClickListener(this);
+                break;
+            case 2:
+                setContentView(R.layout.activity_sign_up_2);
+                man_ic = findViewById(R.id.man_ic);
+                et_phone = findViewById(R.id.user_phone_number);
+                et_pass = findViewById(R.id.user_password);
+                forward = findViewById(R.id.sign_up_forward_2);
+                backward.findViewById(R.id.sign_up_backward_2);
+                forward.setOnClickListener(this);
+                backward.setOnClickListener(this);
+                break;
+            case 3:
+                setContentView(R.layout.activity_sign_up_3);
+                et_car = findViewById(R.id.user_car_number);
+                et_id = findViewById(R.id.user_id_number);
+                backward = findViewById(R.id.sign_up_backward_3);
+                sign_up = findViewById(R.id.bt_sign_up);
+                backward.setOnClickListener(this);
+                sign_up.setOnClickListener(this);
+                break;
+            default:
+                break;
+        }
+    }
+
     private boolean checkValid() {
-        name = et_name.getText().toString();
-        phoneNumber = et_phone.getText().toString();
-        password = et_pass.getText().toString();
-        carNumber = et_car.getText().toString();
-        idNumber = et_id.getText().toString();
-        if(!isChecked){
-            Toast.makeText(this, "请选择性别", Toast.LENGTH_SHORT).show();
-            return false;
-        }else if(name.isEmpty()){
-            Toast.makeText(this, "请填写姓名", Toast.LENGTH_SHORT).show();
-            return false;
-        }else if(phoneNumber.isEmpty()){
-            Toast.makeText(this, "请填写电话号码", Toast.LENGTH_SHORT).show();
-            return false;
-        }else if(password.isEmpty()){
-            Toast.makeText(this, "请填写密码", Toast.LENGTH_SHORT).show();
-            return false;
-        }else if(carNumber.isEmpty()){
-            Toast.makeText(this, "请填写车牌号", Toast.LENGTH_SHORT).show();
-            return false;
-        }else if(idNumber.isEmpty()){
-            Toast.makeText(this, "请填写身份证号", Toast.LENGTH_SHORT).show();
-            return false;
+        switch (viewIndex) {
+            case 1:
+                name = et_name.getText().toString();
+                if (!isChecked) {
+                    Toast.makeText(this, "请选择性别", Toast.LENGTH_SHORT).show();
+                    return false;
+                } else if (name.isEmpty()) {
+                    Toast.makeText(this, "请填写姓名", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                break;
+            case 2:
+                phoneNumber = et_phone.getText().toString();
+                password = et_pass.getText().toString();
+                if (phoneNumber.isEmpty()) {
+                    Toast.makeText(this, "请填写电话号码", Toast.LENGTH_SHORT).show();
+                    return false;
+                } else if (password.isEmpty()) {
+                    Toast.makeText(this, "请填写密码", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                break;
+            case 3:
+                carNumber = et_car.getText().toString();
+                idNumber = et_id.getText().toString();
+                if (carNumber.isEmpty()) {
+                    Toast.makeText(this, "请填写车牌号", Toast.LENGTH_SHORT).show();
+                    return false;
+                } else if (idNumber.isEmpty()) {
+                    Toast.makeText(this, "请填写身份证号", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                break;
+            default:
+                break;
         }
         return true;
     }
 
     private void addUser() {
         LitePal.getDatabase();
-        User user = new User(gender,name,phoneNumber,password,carNumber,idNumber);
+        User user = new User(gender, name, phoneNumber, password, carNumber, idNumber);
         user.save();
     }
 }
